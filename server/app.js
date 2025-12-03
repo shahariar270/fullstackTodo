716787d5d7496a1e3a265c733632609bd58246ec
shahariar270 <mdshahariar102@gmail.com>
2025-12-03 07:15:20 +0600

    connect database

--- server/app.js
+++ server/app.js
@@ -2,20 +2,25 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const router = require('./router');
+const { default: mongoose } = require('mongoose');
 
 app.use(cors());
 app.use(express.json())
 
-const port =3000;
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DB_URL)
+    .then(() => console.log('database connect successfully'))
+    .catch((err) => console.log(err))
 
 app.use('/api', router)
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.status(200).json({
         massage: 'data get successfully',
     })
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('server is ruing');
 })
