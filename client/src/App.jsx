265541042f51ecc151c71e95bf03c504897d5b64
shahariar270 <mdshahariar102@gmail.com>
2025-12-07 20:24:31 +0600

    remove unused code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,7 @@ import { useEffect } from 'react';
 import { apiRoute, fetchApi } from './Ultis/helper';
 
 function App() {
-  const [count, setMessage] = useState(null);
-  const data = {
-    id: Date.now(),
-    date:"22 nov 2025",
-    isComplete:false,
-    title:"learn php"
-  }
+  
 
 
   useEffect(() => {
