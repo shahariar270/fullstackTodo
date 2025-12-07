b2f7968e374219468f1eee9350754b99f5058a21
shahariar270 <mdshahariar102@gmail.com>
2025-12-07 20:42:38 +0600

    inital formik setupd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import { useEffect } from 'react';
 import { apiRoute, fetchApi } from './Ultis/helper';
+import { Field, Form, Formik } from 'formik'
 
 function App() {
+  const handleOnSubmit =(values , {setSubmitting})=>{
+    
+
+  }
 
 
 
@@ -12,6 +17,24 @@ function App() {
 
   return (
     <>
+      <h3>todo app</h3>
+      <Formik
+        initialValues={{
+          title: '',
+          description: '',
+          isCompleted: false,
+        }}
+        onSubmit={handleOnSubmit}
+      >
+        <Form>
+          <Field
+            as='input'
+            name='title'
+            placeholder='enter title'
+          />
+        </Form>
+
+      </Formik>
 
     </>
   )
