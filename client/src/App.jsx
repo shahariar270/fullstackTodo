6518bdbc994f617f46518440908239cf9a637b6b
shahariar270 <mdshahariar102@gmail.com>
2025-12-08 07:06:35 +0600

    default va;lue set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,77 +34,82 @@ function App() {
     loadData();
   }, []);
 
-    return (
-      <div className="todo-app">
-        <h2 className="header">Todo Manager</h2>
+  return (
+    <div className="todo-app">
+      <h2 className="header">Todo Manager</h2>
 
-        <div className="card form-card">
-          <Formik
-            enableReinitialize
-            initialValues={initialValuesData(edit)}
-            onSubmit={handleOnSubmit}
-          >
-            <Form className="form">
-              <Field
-                name="title"
-                placeholder="What do you need to do?"
-                className="input"
-              />
-              <button type="submit" className="btn primary">
-                {edit ? "Update" : "Add"}
-              </button>
-            </Form>
-          </Formik>
-        </div>
-
-        <div className="card list-card">
-          <ul className="list">
-            {data.map((item) => (
-              <li key={item.id} className="list-item">
-                <label className="left">
-                  <input
-                    type="checkbox"
-                    checked={item.isComplete}
-                    onChange={async (e) => {
-                      await axios.put(`${apiRoute}/todos/${item.id}`, {
-                        ...item,
-                        isComplete: e.target.checked,
-                      });
-                      loadData();
-                    }}
-                  />
-                  <span className={item.isComplete ? "done" : ""}>
-                    {item.title}
-                  </span>
-                </label>
-
-                <div className="right">
-                  <input
-                    type="date"
-                    onChange={async (e) => {
-                      await axios.put(`${apiRoute}/todos/${item.id}`, {
-                        ...item,
-                        targetDate: e.target.value,
-                      });
-                      loadData();
-                    }}
-                  />
-                  <button className="btn edit" onClick={() => setEdit(item)}>
-                    Edit
-                  </button>
-                  <button
-                    className="btn delete"
-                    onClick={() => deleteHandle(item.id)}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="card form-card">
+        <Formik
+          enableReinitialize
+          initialValues={initialValuesData(edit)}
+          onSubmit={handleOnSubmit}
+        >
+          <Form className="form">
+            <Field
+              name="title"
+              placeholder="What do you need to do?"
+              className="input"
+            />
+            <button type="submit" className="btn primary">
+              {edit ? "Update" : "Add"}
+            </button>
+          </Form>
+        </Formik>
       </div>
-  );
-  }
 
-  export default App;
+      <div className="card list-card">
+        <ul className="list">
+          {data.map((item) => (
+            <li key={item.id} className="list-item">
+              <label className="left">
+                <input
+                  type="checkbox"
+                  checked={item.isComplete}
+                  onChange={async (e) => {
+                    await axios.put(`${apiRoute}/todos/${item.id}`, {
+                      ...item,
+                      isComplete: e.target.checked,
+                    });
+                    loadData();
+                  }}
+                />
+                <span className={item.isComplete ? "done" : ""}>
+                  {item.title}
+                </span>
+              </label>
+              <div className="right">
+                <input
+                  type="date"
+                  defaultValue={
+                    item.targetDate
+                      ? item.targetDate.slice(0, 10)
+                      : ""
+                  }
+                  onChange={async (e) => {
+                    await axios.put(`${apiRoute}/todos/${item.id}`, {
+                      ...item,
+                      targetDate: e.target.value,
+                    });
+                    loadData();
+                  }}
+                />
+
+                <button className="btn edit" onClick={() => setEdit(item)}>
+                  Edit
+                </button>
+                <button
+                  className="btn delete"
+                  onClick={() => deleteHandle(item.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
