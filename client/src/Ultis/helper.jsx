d48aebfa0be767c736a0c0d15548fe92a4f89180
shahariar270 <mdshahariar102@gmail.com>
2025-12-08 07:13:25 +0600

    remove log

--- client/src/Ultis/helper.jsx
+++ client/src/Ultis/helper.jsx
@@ -5,7 +5,6 @@ export const apiRoute = import.meta.env.API_URL ?? 'http://localhost:3000/api';
 export const fetchApi = async (url) => {
     try {
         const response = await axios.get(`${apiRoute}/${url}`);
-        console.log(response.data);
         return response.data;
     }
     catch (error) {
