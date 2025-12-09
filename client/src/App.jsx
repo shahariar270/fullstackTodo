0c2b323d376e166935357eaf6c242b6d6ea50c50
shahariar270 <mdshahariar102@gmail.com>
2025-12-09 07:10:18 +0600

    added loading state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import axios from 'axios';
 function App() {
   const [data, setData] = useState([]);
   const [edit, setEdit] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const loadData = async () => {
     const res = await fetchApi('todos');
@@ -14,20 +15,24 @@ function App() {
   };
 
   const handleOnSubmit = async (values, { resetForm }) => {
+    setLoading(true)
     if (edit?.id) {
       await axios.put(`${apiRoute}/todos/${edit.id}`, values);
+      setLoading(false)
       setEdit(null);
     } else {
       await axios.post(`${apiRoute}/todos`, values);
+      setLoading(false)
     }
-
-    await loadData();
     resetForm();
   };
 
   const deleteHandle = async (id) => {
-    await axios.delete(`${apiRoute}/todos/${id}`);
-    await loadData();
+    setLoading(true)
+    await axios.delete(`${apiRoute}/todos/${id}`).finally(() => {
+      setLoading(false)
+    })
+
   };
 
   useEffect(() => {
@@ -58,55 +63,59 @@ function App() {
       </div>
 
       <div className="card list-card">
-        <ul className="list">
-          {data.map((item) => (
-            <li key={item.id} className="list-item">
-              <label className="left">
-                <input
-                  type="checkbox"
-                  checked={item.isComplete}
-                  onChange={async (e) => {
-                    await axios.put(`${apiRoute}/todos/${item.id}`, {
-                      ...item,
-                      isComplete: e.target.checked,
-                    });
-                    loadData();
-                  }}
-                />
-                <span className={item.isComplete ? "done" : ""}>
-                  {item.title}
-                </span>
-              </label>
-              <div className="right">
-                <input
-                  type="date"
-                  defaultValue={
-                    item.targetDate
-                      ? item.targetDate.slice(0, 10)
-                      : ""
-                  }
-                  onChange={async (e) => {
-                    await axios.put(`${apiRoute}/todos/${item.id}`, {
-                      ...item,
-                      targetDate: e.target.value,
-                    });
-                    loadData();
-                  }}
-                />
+        {loading ? <span>loading...</span> :
+          <ul className="list">
+            {data.map((item) => (
+              <li key={item.id} className="list-item">
+                <label className="left">
+                  <input
+                    type="checkbox"
+                    checked={item.isComplete}
+                    onChange={async (e) => {
+                      setLoading(true);
+                      await axios.put(`${apiRoute}/todos/${item.id}`, {
+                        ...item,
+                        isComplete: e.target.checked,
+                      }).finally(() => {
+                        setLoading(false)
+                      })
+                    }}
+                  />
+                  <span className={item.isComplete ? "done" : ""}>
+                    {item.title}
+                  </span>
+                </label>
+                <div className="right">
+                  <input
+                    type="date"
+                    defaultValue={
+                      item.targetDate
+                        ? item.targetDate.slice(0, 10)
+                        : ""
+                    }
+                    onChange={async (e) => {
+                      await axios.put(`${apiRoute}/todos/${item.id}`, {
+                        ...item,
+                        targetDate: e.target.value,
+                      });
+                      loadData();
+                    }}
+                  />
 
-                <button className="btn edit" onClick={() => setEdit(item)}>
-                  Edit
-                </button>
-                <button
-                  className="btn delete"
-                  onClick={() => deleteHandle(item.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
+                  <button className="btn edit" onClick={() => setEdit(item)}>
+                    Edit
+                  </button>
+                  <button
+                    className="btn delete"
+                    onClick={() => deleteHandle(item.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        }
       </div>
     </div>
   );
