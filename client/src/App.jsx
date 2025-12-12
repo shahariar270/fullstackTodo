b3838e06dd8daea6728d94a013a8077981b7cf7a
Shahariar <149128436+shahariar270@users.noreply.github.com>
2025-12-12 22:05:09 +0600

    show a massage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -72,55 +72,59 @@ function App() {
       <div className="card list-card">
         {loading ? <span>loading...</span> :
           <ul className="list">
-            {data.map((item) => (
-              <li key={item.id} className="list-item">
-                <label className="left">
-                  <input
-                    type="checkbox"
-                    checked={item.isComplete}
-                    onChange={async (e) => {
-                      setLoading(true);
-                      await axios.put(`${apiRoute}/todos/${item.id}`, {
-                        ...item,
-                        isComplete: e.target.checked,
-                      }).finally(() => {
-                        setLoading(false)
-                      })
-                    }}
-                  />
-                  <span className={item.isComplete ? "done" : ""}>
-                    {item.title}
-                  </span>
-                </label>
-                <div className="right">
-                  <input
-                    type="date"
-                    defaultValue={
-                      item.targetDate
-                        ? item.targetDate.slice(0, 10)
-                        : ""
-                    }
-                    onChange={async (e) => {
-                      await axios.put(`${apiRoute}/todos/${item.id}`, {
-                        ...item,
-                        targetDate: e.target.value,
-                      });
-                      loadData();
-                    }}
-                  />
+            {data?.length === 0 ? <span>you have no todo complete</span>
+              :
+              ( data.map((item) => (
+                  <li key={item.id} className="list-item">
+                    <label className="left">
+                      <input
+                        type="checkbox"
+                        checked={item.isComplete}
+                        onChange={async (e) => {
+                          setLoading(true);
+                          await axios.put(`${apiRoute}/todos/${item.id}`, {
+                            ...item,
+                            isComplete: e.target.checked,
+                          }).finally(() => {
+                            setLoading(false)
+                          })
+                        }}
+                      />
+                      <span className={item.isComplete ? "done" : ""}>
+                        {item.title}
+                      </span>
+                    </label>
+                    <div className="right">
+                      <input
+                        type="date"
+                        defaultValue={
+                          item.targetDate
+                            ? item.targetDate.slice(0, 10)
+                            : ""
+                        }
+                        onChange={async (e) => {
+                          await axios.put(`${apiRoute}/todos/${item.id}`, {
+                            ...item,
+                            targetDate: e.target.value,
+                          });
+                          loadData();
+                        }}
+                      />
 
-                  <button className="btn edit" onClick={() => setEdit(item)}>
-                    Edit
-                  </button>
-                  <button
-                    className="btn delete"
-                    onClick={() => deleteHandle(item.id)}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </li>
-            ))}
+                      <button className="btn edit" onClick={() => setEdit(item)}>
+                        Edit
+                      </button>
+                      <button
+                        className="btn delete"
+                        onClick={() => deleteHandle(item.id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </li>
+                )))
+            }
+
           </ul>
         }
       </div>
