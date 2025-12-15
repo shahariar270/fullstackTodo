c8eceb45d89998d27a437e026285e9da1b2e8e35
shahariar270 <mdshahariar102@gmail.com>
2025-12-15 07:23:12 +0600

    copy server added

--- server/router/index.js
+++ server/router/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-let todos = require('../Data');
+const Todos = require('../Data/index')
 const { getTodos, createTodos, deleteTodos, updateTodo } = require('../control');
 const router = express.Router();
 
@@ -10,5 +10,18 @@ router.get('/todos', getTodos)
 router.delete('/todos/:id', deleteTodos);
 
 router.put('/todos/:id', updateTodo)
+
+router.post('/todos/:id/clone', async (req, res) => {
+    const todoID = await Todos.findById(req.params.id).lean();
+    if (!todoID) {
+        return res.status(404).json({ message: "todo not found" });
+    }
+
+    delete todoID._id;
+    todoID.title += " (copy)";
+
+    const newProduct = await Todos.create(todoID);
+    res.status(201).json(newProduct);
+})
 
 module.exports = router;
