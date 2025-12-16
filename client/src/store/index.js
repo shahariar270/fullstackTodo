8fc57fd5ff74e7d1eed887d5768205932f5b7a59
shahariar270 <mdshahariar102@gmail.com>
2025-12-16 21:15:47 +0600

    setup store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,15 @@
 // src/app/store.js
 import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
+
+const logger = createLogger({
+    collapsed: true,
+    diff: true
+});
 
 export const store = configureStore({
     reducer: {
-       
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(logger),
 });
