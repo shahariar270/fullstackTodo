79b608bb11c5cf0f2ce616ce749fb3c3f721ac48
shahariar270 <mdshahariar102@gmail.com>
2025-12-22 21:59:10 +0600

    set active tabs

--- client/src/component/Tab/index.jsx
+++ client/src/component/Tab/index.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.scss'
 
 export const Tab = ({ tabs }) => {
+  const [ activeTab, setActiveTab ] = useState(0);
+  console.log(activeTab);
+
   return (
     <div className='tab-wrapper'>
       <div className="tab-wrapper--header">
         {tabs.map((tab, index) => (
-          <div key={index} className='tab-item'>
+          <div 
+          key={index} 
+          onClick={()=> setActiveTab(index)}
+          className={`tab-item ${activeTab === index ? 'tab-item--active' : ''}`}
+          >
             {tab.title}
           </div>
         ))}
