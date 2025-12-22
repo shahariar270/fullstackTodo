264f1006a44be174583eacf40b062e2ce0ade863
shahariar270 <mdshahariar102@gmail.com>
2025-12-22 21:43:18 +0600

    some basic style implement

--- client/src/component/Tab/index.jsx
+++ client/src/component/Tab/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import './styles.scss'
 
-export const Tab = ({tabs}) => {
+export const Tab = ({ tabs }) => {
   return (
-    <div>Tab</div>
+    <div className='tab-wrapper'>
+      <div className="tab-wrapper--header">
+        {tabs.map((tab, index) => (
+          <div key={index} className='tab-item'>
+            {tab.title}
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
